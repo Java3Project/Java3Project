eb661cf332e5b28bae79599e46bcb13b476663b8
Wissam-Hawc <wissam.hawcharw@gmail.com>
2024-06-01 21:26:01 +0300

    test

--- NFA035_CODE_GR25/src/Project.java
+++ NFA035_CODE_GR25/src/Project.java
@@ -1,4 +1,21 @@
 
 public class Project {
+    private String name;
 
+    public Project(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
